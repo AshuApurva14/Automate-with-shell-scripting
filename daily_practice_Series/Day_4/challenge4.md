557f0354f0d54dc40180f2d6789fe90edff87140
Ashutosh Apurva <apooorva01@gmail.com>
2025-09-05 04:18:17 +0000

    new: add day4 task

---
+++ daily_practice_Series/Day_4/challenge4.md
@@ -0,0 +1,4 @@
+## Echo with Command Substitution:
+
+ - Write a Bash script that uses command substitution to display the current date and time using the date command.
+
---
+++ daily_practice_Series/Day_4/solution.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# echo suubstitution command demo
+
+set -e
+
+
+
+current_datetime=$(date)
+
+# Displaying the current date and time
+
+echo "Current date and time: $current_datetime"
+
+
