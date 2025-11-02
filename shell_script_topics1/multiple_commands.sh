6091d870c5444b5300836fe29e59f9deb33720d2
Ashutosh Apurva <apooorva01@gmail.com>
2025-11-02 04:11:18 +0000

    shel scripts topic dir

---
+++ shell_script_topics1/multiple_commands.sh
@@ -0,0 +1,6 @@
+# How to execute mulitple commands in Shell/Bash scripting
+
+# 1. `;` is used to separate two commands and execute in a single line in Bash/ Shell Scripting.
+# 2. { } can be used to comment any line in bash/ shell script.
+
+
